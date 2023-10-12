379eb35c9e289461905e7ad5eee60ac0b8ab5683
eunso0713 <aliennumber1one@sookmyung.ac.kr>
2023-10-12 20:53:22 +0900

    5

--- main.c
+++ main.c
@@ -1,14 +1,34 @@
 #include <stdio.h>
 
-void square( int a )
+
+int factorial(int n)
 {
-	a = a*a;
-	
+	int i;
+	int res=1;
+	for(i=1; i<=n; i++)
+	{
+		res*=i;
+	}
+	return res;
 }
 
-int main()
+int C(int n, int r)
 {
-	int a = 2;
-	square(a);
-	printf("a=%i\n", a);
+	return factorial(n)/(factorial(n-r) * factorial(r));
+}
+
+int main(void)
+{
+	int n, r;
+	printf("Enter n: ");
+	scanf("%d", &n);
+	printf("Enter r: ");
+	scanf("%d", &r);
+	
+	
+	int result=C(n, r);
+	printf("result: %d\n", result);
+	
+	
+	return 0;
 }
